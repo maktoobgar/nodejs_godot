18abdae4a5dcded46519b8f5f0077ceb10ec315d
Mahmood Heidari <mahmoodh1378@gmail.com>
2021-06-25 12:08:17 +0430

    ✅ server functionality updated and somehow completed for this repository(I think)

--- server/index.js
+++ server/index.js
@@ -3,13 +3,24 @@ var ws = require('ws')
 var server = new ws.Server({ port: 3333 })
 
 server.on('connection', (client) => {
+    client.send(JSON.stringify({
+        "welcome": true
+    }))
     client.on('message', (message) => {
         let data = JSON.parse(message)
+        server.broadcast(JSON.stringify({
+            'username': data['username'],
+            'text': data['text']
+        }))
         console.log(data)
     })
     client.on('close', (message) => {
-        console.log(code, message)
+        console.log(message)
     })
-
-    client.send(JSON.stringify({'test': 'test'}))
 })
+
+server.broadcast = function broadcast(msg) {
+    server.clients.forEach(function each(client) {
+        client.send(msg);
+     });
+ };
